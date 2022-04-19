e897a7d8e3114718f4c7678828e187259c89aac1
JeromeDLX <jerome.delecroix@hotmail.fr>
2022-04-19 14:13:59 +0200

    Fonction de login user ok avec identifiants unique requis + haschage du password avec Bcrypt

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,14 +1,32 @@
 const {User} = require('../mongo');
+const bcrypt = require('bcrypt');
 
-// Fonction servant à la création d'un utilisateur
-function creationUser(req, res){
+// Fonction servant à la création d'un utilisateur, et au haschage du password
+async function creationUser(req, res){
     const {email, password} = req.body
-    const user = new User({email, password})
-        
+    const cryptagePassword = await hashPassword(password)
+    
+    console.log("password", password)
+    console.log("cryptagePassword", cryptagePassword)
+
+    const user = new User({email, password: cryptagePassword})
+    
     user
     .save()
-    .then(() => res.send({message: "Utilisateur enregistré !"}))
-    .catch ((err) => console.log("Echec lors de l'enregistrement utilisateur", err))
+    .then(() => res.status(201).send({message: "Utilisateur enregistré"}))
+    .catch ((err) => res.status(409).send({message: "Echec lors de l'enregistrement utilisateur" + err}))
 };
 
-module.exports = {creationUser};
+// Fonction qui sert au cryptage des MDP utilisateurs
+function hashPassword(password){
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds)
+};
+
+// Fonction servant à loguer l'utilisateur
+function loginUser(req, res){
+    const email = req.body.email
+    const password = req.body.password
+};
+
+module.exports = {creationUser, loginUser};
--- back/mongo.js
+++ back/mongo.js
@@ -1,20 +1,26 @@
-// Connexion à la base de données MongoDB, utilisation fichier .env
+// Import pour la connexion à la base de données MongoDB
 const mongoose = require('mongoose');
+// Import du package pour la validation d'identifiants unique
+const uniqueValidator = require('mongoose-unique-validator')
+
+// Utilisation fichier .env pour l'uri vers la base de données
 const password = process.env.DB_PASSWORD;
-const username = process.env.DB_USER
-const database = process.env.DB_DATABASE
+const username = process.env.DB_USER;
+const database = process.env.DB_DATABASE;
 const uri = `mongodb+srv://${username}:${password}@cluster-piiquante.rsq6v.mongodb.net/${database}?retryWrites=true&w=majority`;
 
+// Connexion à mongoDB avec l'URI fournie
 mongoose
 .connect(uri)
 .then(() => console.log("Connecté à Mongo"))
 .catch ((err) => console.log("Echec de la connexion à Mongo", err));
 
-// Construction d'un schéma typique d'un utilisateur (Informations recueillis)
+// Construction d'un schéma typique d'un utilisateur (Informations recueillis), identifiants uniques
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
+    email: {type: String, require: true, unique: true},
+    password: {type: String, required: true},
 });
+userSchema.plugin(uniqueValidator);
 
 const User = mongoose.model("User", userSchema);
 
